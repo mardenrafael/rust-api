e030b72657e86c4b88421f655a2484bdff61d372
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-02-28 21:24:06 -0400

    config rocket framework

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+#![feature(decl_macro)]
+
 #[macro_use] extern crate rocket;
+
+pub mod controllers;
+pub mod services;
 
 
 
 fn config_rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![])
+    rocket::ignite()
+        .mount("/api", routes![])
 }
 fn main() {
     config_rocket().launch();
