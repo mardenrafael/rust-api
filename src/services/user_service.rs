e6774932c075a7b80d251df2dedb64e351469a04
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-02-28 21:23:53 -0400

    create user service

---
+++ src/services/user_service.rs
@@ -0,0 +1,26 @@
+pub struct UserService {
+
+}
+
+impl UserService {
+
+    /// Get all users
+    pub fn get_all_users() -> () {
+        todo!("get user not implemented")
+    }
+    
+    /// Get user by id
+    pub fn get_user_by_id() -> () {
+        todo!("get user by id not implemented")
+    }
+    
+    /// Delete user locate by id
+    pub fn delete_user_by_id() -> () {
+        todo!("get user by id not implemented")
+    }
+
+    /// Update user localate by id
+    pub fn update_user_by_id() -> () {
+        todo!("update user by id not implemented")
+    }   
+}
